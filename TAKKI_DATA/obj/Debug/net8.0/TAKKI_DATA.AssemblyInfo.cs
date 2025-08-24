64f62510a6d14be340a7bbf6f453706047caf8a7
Angel Encarnacion <angelm7encarnacion@gmail.com>
2025-08-24 14:04:39 -0400

    WIP

--- TAKKI_DATA/obj/Debug/net8.0/TAKKI_DATA.AssemblyInfo.cs
+++ TAKKI_DATA/obj/Debug/net8.0/TAKKI_DATA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TAKKI_DATA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bacf30e5c104660b2fe90fb5eaa5785c7f4762a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TAKKI_DATA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TAKKI_DATA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
